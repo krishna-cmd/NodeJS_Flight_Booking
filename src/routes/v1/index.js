746365be8e57489f6e98eef7385b6c9464cf81a0
KRISHNA BIRADAR <59815092+krishna-cmd@users.noreply.github.com>
2026-01-08 15:46:08 +0530

    Revert "added basic express functionalities"
    This reverts commit b466e1adb454263d0cdcf04c247dff64fa6d088c.

--- src/routes/v1/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { infoController } = require("../../controllers");
-
-const router = express.Router();
-
-router.get("/info", infoController.info);
-
-module.exports = router;
